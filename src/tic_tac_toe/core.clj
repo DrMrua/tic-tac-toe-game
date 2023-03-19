f29919f62b697e068ca55de3579ed154b102a130
Mathias Andersen <maddeeking@live.dk>
2023-03-19 16:33:49 +0100

    game is working

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,4 +1,5 @@
-(ns tic-tac-toe.core)
+(ns tic-tac-toe.core
+  (:gen-class))
 
 (def initial-board [[1 2 3] [4 5 6] [7 8 9]])
 
@@ -41,14 +42,13 @@
     (conj [] from-top-left from-top-right)))
 
 (defn winner? [board player]
-  (let [win-on-row? (some (fn [row] (every? (fn [e] (= e player)) row)) board)
-        row board
+  (let [row board
         column (convert-to-column-order board)
         diagonal (convert-to-diagonal-order board)
         fun (->> row
                  (concat column diagonal)
                  vec
-                 (some (fn [row] (every? (fn [e] (= e player)) row))))]
+                 (some (fn [r] (every? (fn [e] (= e player)) r))))]
     fun))
 
 (defn tie?
@@ -56,28 +56,15 @@
   (let [full-board? (every?
                       (fn [row] (not-any? number? row))
                       board)
-        available-space (reduce
-                          (fn [acc r]
-                            (+ acc
-                               (reduce
-                                 (fn [acc x] (if (number? x) (inc acc) 0))
-                                 0
-                                 r)))
-                          0
-                          board)]
-    ;(if (<= available-space 1)
-    ;  (do
-    ;    (println "The game is a tie!!!. Start the game to play again.")
-    ;    true)
-    ;  false)
-    full-board?
-    ))
+        ]
+    full-board?))
 
 (defn update-board [input board player-x-turn?]
   (cond
     (contains? #{1 2 3} input) (assoc board 0 (assoc (get-in board [0]) (rem (dec input) 3) (player-turn player-x-turn?)))
     (contains? #{4 5 6} input) (assoc board 1 (assoc (get-in board [1]) (rem (dec input) 3) (player-turn player-x-turn?)))
     (contains? #{7 8 9} input) (assoc board 2 (assoc (get-in board [2]) (rem (dec input) 3) (player-turn player-x-turn?)))))
+
 (defn valid-number?
   "Returns true if the `input` is an integer between 1-9, else returns false.
   If the input is a string an exception is thrown"
@@ -123,41 +110,14 @@
          player-x-turn? true]
     (let [print-the-board (print-board board)]
       (cond
-        (winner? board (player-turn player-x-turn?)) (str "Congratulations you have won the game!")
+        (winner? board (player-turn (not player-x-turn?))) (str "Congratulations you have won the game!")
         (tie? board) (str "It is a tie!")
         :else
         (recur
           (move board (take-turn player-x-turn?))
           (not player-x-turn?))))))
 
-#_(defn start
-  []
-  (println "You have started a new game. Press a number between 1-9")
-  (do
-    ;(read-input)
-    (loop [board initial-board                                ;state of board
-           player-x-turn? false                                ;shows players turn it is.
-           print-b (print-board board)
-           ]
-      (let [
-            ;print-board (print-board board)
-            input (read-input)
-            ]
-        (cond
-          (= input "stop") (str "game has stopped")               ;End the game if user writes stop.
-          (winner? board (player-turn player-x-turn?)) (do
-                                                         (print-board board)
-                                                         (str "You have won the game!"))
-          (tie? board)    (do
-                            (print-board board)
-                            (str "The game is a tie"))
-          (and (valid-number? input) (valid-field? input board)) (recur
-                                                                   (move input board player-x-turn?) ;state of board
-                                                                   (take-turn (not player-x-turn?))
-                                                                   (print-board board)) ;each player take turns
-          :else
-          (recur
-            board
-            (take-turn player-x-turn?)                        ;take turn.
-            (print-board board)
-            ))))))
+(defn -main
+  [& args]
+  (start-game))
+
